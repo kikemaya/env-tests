9774968a2c41ab4efa2ceb3fe509cdcf4cdf8230
kikemaya <enrique.mg.cd@gmail.com>
2025-04-28 19:30:01 -0600

    Another env added... again

--- src/app/page.js
+++ src/app/page.js
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Another env variable {process.env.EXMPL}
+            Another env variable {process.env.EXMPL}!
           </li>
         </ol>
 
